49b6ab75c5f9f15c2644985e7cb58cafa7e98ca9
Ebire Folayemi Michael <folayemiebire@gmail.com>
2023-10-16 23:08:10 +0100

    updating my python package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Python Flask setup library'
 LONG_DESCRIPTION = 'A package that simplifies the creation of Python Flask applications with a pre-configured project structure.'
 
