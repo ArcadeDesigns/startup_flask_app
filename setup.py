805658e7b34a2ff272fefcd2323c5a1e8cc81c6a
Ebire Folayemi Michael <84265954+ArcadeDesigns@users.noreply.github.com>
2023-10-16 23:10:16 +0100

    Update setup.py
    Updating package version to 1.0.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+VERSION = '1.0.1'
 DESCRIPTION = 'Python Flask setup library'
 LONG_DESCRIPTION = 'A package that simplifies the creation of Python Flask applications with a pre-configured project structure.'
 
